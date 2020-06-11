e6591cd60d6d5becc2ed6b41292b0776ac4c74fd
hafffe <aronhafner@gmail.com>
2020-06-11 20:08:27 +0200

    Set lang attribute on html tag

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, {Html, Head, Main, NextScript} from 'next/document';
 
 export default class MyDocument extends Document {
 	render = (): JSX.Element => (
-		<Html>
+		<Html lang='en'>
 			<Head>
 				<link
 					href='https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;1,200;1,300;1,400;1,600;1,700&display=swap'
