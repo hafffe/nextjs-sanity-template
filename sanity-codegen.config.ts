4114334cc1c49272b51cc17074686a775b782bf9
hafffe <aronhafner@gmail.com>
2020-12-22 23:56:38 +0100

    npm install

--- sanity-codegen.config.ts
+++
@@ -1,8 +0,0 @@
-import {SanityCodegenConfig} from 'sanity-codegen';
-
-const config: SanityCodegenConfig = {
-	schemaPath: './studio/schemas/schema',
-	outputPath: './schema.ts'
-};
-
-export default config;
