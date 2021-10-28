a5e5f03a9a5a9d37776165d296dab8f15d64e99b
hafffe <aronhafner@gmail.com>
2021-10-29 00:25:16 +0200

    Use next-lint in favor for xo

--- components/common/head.tsx
+++ components/common/head.tsx
@@ -1,16 +1,8 @@
-import NextHead from 'next/head';
 import {DefaultSeo} from 'next-seo';
 import SEO from '../../next-seo.config';
 
 const Head = () => (
 	<>
-		<NextHead>
-			<meta charSet='utf-8' />
-			<meta content='IE=edge' httpEquiv='X-UA-Compatible' />
-			<meta content='width=device-width, initial-scale=1' name='viewport' />
-			<link rel='icon' type='image/svg+xml' href='favicon.svg' />
-			<link href='https://fonts.googleapis.com/css2?family=Work+Sans' rel='stylesheet' />
-		</NextHead>
 		<DefaultSeo {...SEO} />
 	</>
 );
