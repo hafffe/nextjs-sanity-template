77a8ecac231e51f5f3db350cd35642f5fb1b54c3
hafffe <aronhafner@gmail.com>
2021-10-28 23:24:00 +0200

    Add preview to youtube

--- studio/schemas/sections/youtube.ts
+++ studio/schemas/sections/youtube.ts
@@ -1,4 +1,14 @@
-import {RiYoutubeLine} from 'react-icons/ri';
+import { RiYoutubeLine } from 'react-icons/ri';
+import YouTube from 'react-youtube';
+
+const Preview = ({ value }) => {
+	const { url } = value;
+	console.log('url', url);
+
+	if (!value.url) {
+		return 'Please enter a URL';
+	}
+};
 
 export default {
 	name: 'youtube',
@@ -10,19 +20,25 @@ export default {
 			name: 'url',
 			type: 'url',
 			title: 'YouTube video URL',
-			validation: (Rule: any) => Rule.required()
+			validation: (Rule: any) => Rule.required(),
 		},
 		{
 			name: 'autoPlay',
 			type: 'boolean',
 			title: 'Enable autoplay',
-			validation: (Rule: any) => Rule.required()
+			validation: (Rule: any) => Rule.required(),
 		},
 		{
 			name: 'muted',
 			type: 'boolean',
 			title: 'Muted',
-			validation: (Rule: any) => Rule.required()
-		}
-	]
+			validation: (Rule: any) => Rule.required(),
+		},
+	],
+	preview: {
+		select: {
+			url: 'url',
+		},
+		component: Preview,
+	},
 };
