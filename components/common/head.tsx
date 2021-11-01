894c9e74512a6f720723024f03dc1dbea185bd3d
Aron Hafner <aronhafner@gmail.com>
2021-11-01 21:41:04 +0100

    Bump studio deps (#521)

--- components/common/head.tsx
+++ components/common/head.tsx
@@ -1,8 +1,12 @@
+import NextHead from 'next/head';
 import {DefaultSeo} from 'next-seo';
 import SEO from '../../next-seo.config';
 
 const Head = () => (
 	<>
+		<NextHead>
+			<meta content='width=device-width, initial-scale=1' name='viewport' />
+		</NextHead>
 		<DefaultSeo {...SEO} />
 	</>
 );
