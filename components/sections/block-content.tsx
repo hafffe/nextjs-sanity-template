bda843a2429a8d4fa32eb961ee69e5b333faab09
hafffe <aronhafner@gmail.com>
2022-02-20 19:10:10 +0100

    Use `@portabletext/react`

--- components/sections/block-content.tsx
+++ components/sections/block-content.tsx
@@ -1,6 +1,6 @@
 import NextLink from 'next/link';
 import {Box, Flex, Heading, Link, Text} from '@chakra-ui/react';
-import BlockContent from '@sanity/block-content-to-react';
+import {PortableText} from '@portabletext/react';
 import {IoIosQuote} from 'react-icons/io';
 import {BlockContent as BlockContentType} from '@/models/sections/block-content';
 import {SimpleBlockContent} from '@/models/objects/simple-block-content';
@@ -10,69 +10,105 @@ type Props = {
 	data: BlockContentType | SimpleBlockContent;
 };
 
-const resolveSize = (style: string | boolean) => {
-	if (style === 'h1') {
-		return '2xl';
+const TextBlock = ({data}: Props) => {
+	if (!data.text) {
+		return null;
 	}
 
-	if (style === 'h2') {
-		return 'xl';
-	}
+	const blocks = data.text;
 
-	if (style === 'h3') {
-		return 'lg';
-	}
-
-	if (style === 'h4') {
-		return 'md';
-	}
-
-	return 'sm';
+	return (
+		<PortableText
+			value={blocks}
+			components={{
+				types: {
+					image: ({value}) => <MainImage data={value} width={960} height={600} />
+				},
+				marks: {
+					link: ({children, value}) => (
+						<NextLink passHref href={`${value.href}`}>
+							<Link>{children}</Link>
+						</NextLink>
+					),
+					italic: ({children}) => <Text as='i'>{children}</Text>
+				},
+				block: {
+					h1: ({children}) => (
+						<Heading as='h1' size='2xl'>
+							{children}
+						</Heading>
+					),
+					h2: ({children}) => (
+						<Heading as='h2' size='xl'>
+							{children}
+						</Heading>
+					),
+					h3: ({children}) => (
+						<Heading as='h3' size='lg'>
+							{children}
+						</Heading>
+					),
+					h4: ({children}) => (
+						<Heading as='h2' size='xl'>
+							{children}
+						</Heading>
+					),
+					h5: ({children}) => (
+						<Heading as='h2' size='xl'>
+							{children}
+						</Heading>
+					),
+					h6: ({children}) => (
+						<Heading as='h2' size='xl'>
+							{children}
+						</Heading>
+					),
+					'h1+center': ({children}) => (
+						<Heading as='h1' size='2xl' textAlign='center'>
+							{children}
+						</Heading>
+					),
+					'h2+center': ({children}) => (
+						<Heading as='h2' size='xl' textAlign='center'>
+							{children}
+						</Heading>
+					),
+					'h3+center': ({children}) => (
+						<Heading as='h3' size='lg' textAlign='center'>
+							{children}
+						</Heading>
+					),
+					'h4+center': ({children}) => (
+						<Heading as='h2' size='md' textAlign='center'>
+							{children}
+						</Heading>
+					),
+					'h5+center': ({children}) => (
+						<Heading as='h2' size='sm' textAlign='center'>
+							{children}
+						</Heading>
+					),
+					'h6+center': ({children}) => (
+						<Heading as='h2' size='sm' textAlign='center'>
+							{children}
+						</Heading>
+					),
+					blockquote: ({children}) => (
+						<Flex paddingY={1}>
+							<Box as='blockquote' display='flex'>
+								<Box as={IoIosQuote} />
+								<Text paddingLeft={2} fontSize='lg' fontWeight='semibold'>
+									{children}
+								</Text>
+							</Box>
+						</Flex>
+					),
+					normal: ({children}) => <Text>{children}</Text>,
+					'normal+center': ({children}) => <Text textAlign='center'>{children}</Text>
+				}
+			}}
+		/>
+	);
 };
-
-const serializers = {
-	types: {
-		block: (props: any) => {
-			const {style = 'normal'} = props.node;
-			const center = /center/.test(style);
-			const heading = /^h\d/.test(style) && style.match(/^h\d/)[0];
-			const size = resolveSize(heading);
-
-			if (heading !== false) {
-				return (
-					<Heading textAlign={center ? 'center' : 'left'} as={heading} size={size}>
-						{props.children}
-					</Heading>
-				);
-			}
-
-			if (style === 'blockquote') {
-				return (
-					<Flex paddingY={1}>
-						<Box as='blockquote' display='flex'>
-							<Box as={IoIosQuote} />
-							<Text paddingLeft={2} fontSize='lg' fontWeight='semibold'>
-								{props.children}
-							</Text>
-						</Box>
-					</Flex>
-				);
-			}
-
-			return <Text textAlign={center ? 'center' : 'left'}>{props.children}</Text>;
-		},
-		image: (props: any) => <MainImage data={props.node} width={960} height={600} />
-	},
-	marks: {
-		link: (props: any) => (
-			<NextLink passHref href={`${props.mark.href}`}>
-				<Link>{props.children}</Link>
-			</NextLink>
-		),
-		italic: (props: any) => <Text as='i'>{props.children}</Text>
-	}
-};
-
-const TextBlock = ({data}: Props) => <BlockContent blocks={data?.text} serializers={serializers} />;
 
 export default TextBlock;
