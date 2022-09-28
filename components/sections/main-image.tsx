56d08f7b84983c9ffbc5a61a4ec5680870514520
Aron Hafner <aronhafner@gmail.com>
2022-09-28 19:33:00 +0200

    Use next-sanity-image for main image (#768)

--- components/sections/main-image.tsx
+++ components/sections/main-image.tsx
@@ -1,7 +1,8 @@
 import {chakra, Box} from '@chakra-ui/react';
 import NextImage from 'next/image';
+import {useNextSanityImage} from 'next-sanity-image';
 import {Image as MainImage} from '@/models/sections/image';
-import {urlFor} from '@/lib/sanity';
+import {urlFor, sanityClient} from '@/lib/sanity';
 
 type Props = {
 	data: MainImage;
@@ -11,6 +12,7 @@ type Props = {
 
 const ImageBlock = ({data, width = 1152, height = 740}: Props) => {
 	const url = data.asset && urlFor(data.asset).width(width).height(height).auto('format').url();
+	const imageProps = useNextSanityImage(sanityClient, data.asset);
 
 	if (!url) {
 		return null;
@@ -22,7 +24,14 @@ const ImageBlock = ({data, width = 1152, height = 740}: Props) => {
 
 	return (
 		<Box marginY={2}>
-			<Image src={url} alt={data?.alt ?? 'Image'} borderRadius='md' width={width} height={height} layout='intrinsic' />
+			<Image
+				{...imageProps}
+				alt={data?.alt ?? 'Image'}
+				borderRadius='md'
+				width={width}
+				height={height}
+				layout='intrinsic'
+			/>
 		</Box>
 	);
 };
