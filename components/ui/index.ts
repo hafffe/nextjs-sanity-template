3f246266e33405f22053ea14ebdcd90f504a56a9
Aron Hafner <aronhafner@gmail.com>
2023-01-06 20:48:07 +0100

    Add footer (#846)

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -3,3 +3,4 @@ export {default as Heading} from './heading';
 export {default as Date} from './date';
 export {default as MaxWidthWrapper} from './max-width-wrapper';
 export {default as Tag} from './tag';
+export {default as Footer} from './footer';
