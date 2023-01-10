ce5dd363fcedb328b7072eab4362df2033e44d2b
hafffe <aronhafner@gmail.com>
2023-01-10 19:06:20 +0100

    Update sanity api version

--- lib/sanity/client.ts
+++ lib/sanity/client.ts
@@ -6,7 +6,7 @@ export const config = {
 	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production',
 	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? 'undefined',
 	useCdn: process.env.NODE_ENV === 'production',
-	apiVersion: '2021-06-07'
+	apiVersion: '2022-11-15'
 };
 
 export const urlForImage = (source: SanityImageSource) => createImageUrlBuilder(config).image(source);
