3646ae2952812515fb1ca82337e363742920cfec
hafffe <aronhafner@gmail.com>
2023-02-19 11:44:39 +0100

    Add generateStaticParams function

--- app/(blog)/(category)/[slug]/page.tsx
+++ app/(blog)/(category)/[slug]/page.tsx
@@ -1,9 +1,18 @@
 import {previewData} from 'next/headers';
-import {pageQuery} from '~/lib/queries';
+import {pageQuery, allPagesSlug} from '~/lib/queries';
 import {sanityClient} from '~/lib/sanity/client';
 import {PageLayout} from '~/components/layout';
 import {PagePreview, PreviewSuspense} from '~/components/previews';
 import type {Page} from '~/models/page';
+
+export const generateStaticParams = async () => {
+	const pageData = await sanityClient.fetch<Page[]>(allPagesSlug);
+
+	return pageData.map((page) => ({
+		slug: page
+	}));
+};
+
 
 const SlugRoute = async ({params}: {params: {slug: string}}) => {
 	const page = await sanityClient.fetch<Page>(pageQuery, {
