d735a7ea82de3025cfe998b90d6eb5f9da1a8b3f
Aron Hafner <aronhafner@gmail.com>
2023-02-19 12:08:17 +0100

    Bump dependencies & add generateStaticParams (#883)

--- app/(blog)/posts/[slug]/page.tsx
+++ app/(blog)/posts/[slug]/page.tsx
@@ -1,9 +1,19 @@
 import {previewData} from 'next/headers';
-import {postQuery} from '~/lib/queries';
+import {postQuery, postsQuery} from '~/lib/queries';
 import {sanityClient} from '~/lib/sanity/client';
 import {PostPageLayout} from '~/components/layout';
 import {PreviewSuspense, PostPreview} from '~/components/previews';
 import type {Post} from '~/models/post';
+
+export const generateStaticParams = async () => {
+	const postsData = await sanityClient.fetch<Post[]>(postsQuery, {
+		limit: 100
+	});
+
+	return postsData.map((post) => ({
+		slug: post.slug.current
+	}));
+};
 
 const PostRoute = async ({params}: {params: {slug: string}}) => {
 	const post = await sanityClient.fetch<Post>(postQuery, {
