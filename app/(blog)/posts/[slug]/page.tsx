af230bdda4df501f7c02a0b6f64e7b9ab5a346a7
Aron Hafner <aronhafner@gmail.com>
2023-03-19 21:11:46 +0100

    Update dependencies & convert to pnpm (#908)

--- app/(blog)/posts/[slug]/page.tsx
+++ app/(blog)/posts/[slug]/page.tsx
@@ -1,6 +1,7 @@
+import type {Metadata} from 'next';
 import {previewData} from 'next/headers';
 import {postQuery, postsQuery} from '~/lib/queries';
-import {sanityClient} from '~/lib/sanity/client';
+import {sanityClient, urlForImage} from '~/lib/sanity/client';
 import {PostPageLayout} from '~/components/layout';
 import {PreviewSuspense, PostPreview} from '~/components/previews';
 import type {Post} from '~/models/post';
@@ -13,6 +14,36 @@ export const generateStaticParams = async () => {
 	return postsData.map((post) => ({
 		slug: post.slug.current
 	}));
+};
+
+export const generateMetadata = async ({params}: {params: {slug: string}}): Promise<Metadata> => {
+	const post = await sanityClient.fetch<Post>(postQuery, {
+		slug: params.slug
+	});
+
+	const ogImage =
+		(post.meta?.openGraphImage &&
+			urlForImage(post.meta.openGraphImage).width(800).height(600).fit('crop').url()) ??
+		'';
+
+	return {
+		title: post.meta?.metaTitle ?? post.title,
+		description: post.meta?.metaDescription,
+		icons: {
+			icon: '/favicon/favicon.svg'
+		},
+		openGraph: {
+			title: post.meta?.openGraphTitle,
+			description: post.meta?.openGraphDescription,
+			images: [
+				{
+					url: ogImage,
+					width: 800,
+					height: 600
+				}
+			]
+		}
+	};
 };
 
 const PostRoute = async ({params}: {params: {slug: string}}) => {
--- app/(blog)/posts/page.tsx
+++ app/(blog)/posts/page.tsx
@@ -1,9 +1,39 @@
+import type {Metadata} from 'next';
 import {pageQuery, postsQuery} from '~/lib/queries';
-import {sanityClient} from '~/lib/sanity/client';
+import {sanityClient, urlForImage} from '~/lib/sanity/client';
 import {PostList} from '~/components/shared';
 import {RenderSection} from '~/components/sections';
 import type {Page} from '~/models/page';
 import type {Post} from '~/models/post';
+
+export const generateMetadata = async (): Promise<Metadata> => {
+	const page = await sanityClient.fetch<Page>(pageQuery, {
+		slug: 'posts'
+	});
+
+	const ogImage =
+		(page.meta?.openGraphImage && urlForImage(page.meta.openGraphImage).width(800).height(600).fit('crop').url()) ??
+		'';
+
+	return {
+		title: page.meta?.metaTitle ?? page.title,
+		description: page.meta?.metaDescription,
+		icons: {
+			icon: '/favicon/favicon.svg'
+		},
+		openGraph: {
+			title: page.meta?.openGraphTitle,
+			description: page.meta?.openGraphDescription,
+			images: [
+				{
+					url: ogImage,
+					width: 800,
+					height: 600
+				}
+			]
+		}
+	};
+};
 
 const IndexPage = async () => {
 	const pageData = await sanityClient.fetch<Page>(pageQuery, {
